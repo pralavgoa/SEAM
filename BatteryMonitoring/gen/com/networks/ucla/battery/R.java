d202f7387656f1fb91cdf102a0c535f9f14c605a
vidhyashri <vidhyashri.prakash@gmail.com>
2012-11-05 22:19:49 -0800

    Added Face Recognition code

--- BatteryMonitoring/gen/com/networks/ucla/battery/R.java
+++ BatteryMonitoring/gen/com/networks/ucla/battery/R.java
@@ -13,6 +13,13 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int test_1=0x7f020002;
+        public static final int test_2=0x7f020003;
+        public static final int test_3=0x7f020004;
+        public static final int test_4=0x7f020005;
+        public static final int test_5=0x7f020006;
+        public static final int test_6=0x7f020007;
+        public static final int test_7=0x7f020008;
     }
     public static final class id {
         public static final int output=0x7f060000;
@@ -23,6 +30,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int intent_face_detect=0x7f040004;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_battery=0x7f040003;
     }
